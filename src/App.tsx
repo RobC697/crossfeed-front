0dd20edb81f302a3f1c22d173772705feaea69fb
chandlergach <chandler@dds.mil>
2020-03-19 11:21:47 -0400

    initial commit
    installed basic dependencies and followed instructions on usdws, see Readme for more details

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Crossfeed</h1>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Homepage</h2>;
+}
+
